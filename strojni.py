cbce7b67a75dd9393f23a822b7d2ad74a8eab761
LukasProchazka1997 <TVA@post.cz>
2025-11-03 18:06:33 +0100

    Update strojni.py

--- strojni.py
+++ strojni.py
@@ -80,7 +80,7 @@ def odesli_email(radek, odpoved, cas, fotka=None):
 # --------------------
 # Hlavní Streamlit aplikace
 # --------------------
-def spojova_app(key_prefix="spojova"):
+def stroiní_app(key_prefix="strojni"):
     radky = nacti_csv(CSV_FILE)
     jmena = nacti_csv(JMENA_FILE)
 
