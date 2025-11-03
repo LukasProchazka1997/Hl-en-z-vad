df76805382a97baaddbb5119d894d7e638458a19
LukasProchazka1997 <TVA@post.cz>
2025-11-03 16:30:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import streamlit as st
-from spojova import Aplikace as SpojovaApp
-from technicka import Aplikace as TechnickaApp
-from strojni import Aplikace as StrojniApp
-from manager import Manager
 
+# Import Streamlit verze jednotlivých aplikací
+from spojova import spojova_app
+from technicka import technicka_app
+from strojni import strojni_app
+from manager import manager_app  # pokud máš Streamlit Manager verzi
 
 # Hlavní nadpis
 st.set_page_config(page_title="Hlášení závad", layout="wide")
 st.title("Hlášení závad")
 
-# Záložky místo Tkinter notebooku
+# Záložky
 tabs = st.tabs(["Spojová služba", "Technická služba", "Strojní služba", "Manager"])
 
 # --- SPOJOVÁ SLUŽBA ---
 with tabs[0]:
     st.subheader("Spojová služba")
-    SpojovaApp(st)
+    spojova_app()
 
 # --- TECHNICKÁ SLUŽBA ---
 with tabs[1]:
     st.subheader("Technická služba")
-    TechnickaApp(st)
+    technicka_app()
 
 # --- STROJNÍ SLUŽBA ---
 with tabs[2]:
     st.subheader("Strojní služba")
-    StrojniApp(st)
+    strojni_app()
 
 # --- MANAGER ---
 with tabs[3]:
     st.subheader("Manager")
-    Manager(st)
+    manager_app()
